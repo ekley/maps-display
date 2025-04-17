f0164fc2e9aacb270bfae75d364f260db7f26137
beneki <benjamin.ekramiand@gmail.com>
2025-04-17 13:12:55 +0100

    add azure maps

---
+++ MapsDisplay/MapsDisplay/wwwroot/js/azureMapsInterop.js
@@ -0,0 +1,18 @@
+﻿window.azureMaps = {
+    map: null,
+    polygonLayer: null,
+    createMap: function (elementId, mapsKey) {
+        this.map = new atlas.Map(elementId, {
+            center: [-0.1276, 51.5074],  // London coordinates
+            zoom: 14,
+            view: 'Auto',
+            language: 'en-US',
+            authType: 'subscriptionKey',
+            subscriptionKey: mapsKey
+        });
+
+        this.map.events.add('ready', () => {
+            // TO-DO
+        });
+    }
+};

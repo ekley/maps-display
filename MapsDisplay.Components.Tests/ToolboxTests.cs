33adedcc65d5053403359397c2113705caab4b28
beneki <benjamin.ekramiand@gmail.com>
2025-04-17 23:28:23 +0100

    add components testing project

---
+++ MapsDisplay.Components.Tests/ToolboxTests.cs
@@ -0,0 +1,50 @@
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MapsDisplay.Client.Components;
+using Shared.Models;
+using Shared.Services.Interfaces;
+
+namespace MapsDisplay.Components.Tests
+{
+    public class ToolboxTests : TestContext
+    {
+        private readonly Mock<ILocalAuthorityApiService> _mockAuthorityService;
+
+        public ToolboxTests()
+        {
+            _mockAuthorityService = new Mock<ILocalAuthorityApiService>();
+            Services.AddSingleton(_mockAuthorityService.Object);
+        }
+
+        [Fact]
+        public void Toolbox_Rendering()
+        {
+            var cut = RenderComponent<Toolbox>();
+            var toggleButton = cut.Find("#toggleLayer");
+            var searchInput = cut.Find(".search-box input");
+
+            Assert.NotNull(toggleButton);
+            Assert.NotNull(searchInput);
+        }
+
+        [Fact]
+        public async Task DebouncedSearchClick_Should_Call_BboxByNameAsync()
+        {
+
+            string testQuery = "Ox";
+            _mockAuthorityService.Setup(service => service.BboxByNameAsync("Oxford"))
+                .ReturnsAsync(new GeometryDto());
+            
+            var ToolBoxComponent = RenderComponent<Toolbox>();
+            var searchBtn = ToolBoxComponent.Find("#search");
+
+            ToolBoxComponent.Find("input[type='text']").Change(testQuery);
+            searchBtn.Click();
+
+            await Task.Delay(800);
+            _mockAuthorityService.Verify(service => service.BboxByNameAsync(testQuery), Times.Once);
+        }
+
+    }
+}

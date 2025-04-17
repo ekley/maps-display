57bcfc60c24c6204e4a26988e59460c6b31bd9ad
beneki <benjamin.ekramiand@gmail.com>
2025-04-17 13:51:22 +0100

    add tile-based rendering for map

--- MapsDisplay/MapsDisplay.Client/Program.cs
+++ MapsDisplay/MapsDisplay.Client/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
--- MapsDisplay/MapsDisplay/wwwroot/js/azureMapsInterop.js
+++ MapsDisplay/MapsDisplay/wwwroot/js/azureMapsInterop.js
@@ -1,7 +1,7 @@
 ﻿window.azureMaps = {
     map: null,
     polygonLayer: null,
-    createMap: function (elementId, mapsKey) {
+    createMap: (elementId, mapsKey) => {
         this.map = new atlas.Map(elementId, {
             center: [-0.1276, 51.5074],  // London coordinates
             zoom: 14,
@@ -14,5 +14,22 @@
         this.map.events.add('ready', () => {
             // TO-DO
         });
+    },
+    addGeoJsonData: () => {
+        // Create a VectorTileSource to load tiles from your API endpoint
+        const source = new atlas.source.VectorTileSource('localAuthorities', {
+            tiles: ['https://localhost:7177/api/tiles/{z}/{x}/{y}.pbf'],
+            maxZoom: 22,
+        });
+        const polygonLayer = new atlas.layer.PolygonLayer(source, null, {
+            "source-layer": 'local_authorities_layer',
+            sourceLayer: 'local_authorities_layer',
+            fillColor: 'rgba(0, 100, 255, 0.4)',
+            strokeColor: '#004aad',
+            strokeWidth: 2
+        });
+
+        this.map.sources.add(source);
+        this.map.layers.add(polygonLayer);
     }
 };

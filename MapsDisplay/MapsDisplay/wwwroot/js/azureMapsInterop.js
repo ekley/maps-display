26bd4ce5024aec13b6f5c07262b13f29887d1136
beneki <benjamin.ekramiand@gmail.com>
2025-04-22 15:21:40 +0100

    Refactor: remove unnecessary code and optimize

--- MapsDisplay/MapsDisplay/wwwroot/js/azureMapsInterop.js
+++ MapsDisplay/MapsDisplay/wwwroot/js/azureMapsInterop.js
@@ -1,6 +1,6 @@
 ﻿window.azureMaps = {
     map: null,
-    currentLayer: null,
+    currentLayerId: null,
 
     createMap: function (elementId, mapsKey) {
         this.map = new atlas.Map(elementId, {
@@ -17,16 +17,14 @@
         });
     },
     clearMap: function () {
-        if (!this.map || !this.currentLayer) return;
-
-        const layerId = this.currentLayer.id;
-        const doesLayerExist = this.map.layers._getMapboxLayerExists(layerId);
+        if (!this.map || !this.currentLayerId) return;
+        const doesLayerExist = this.map.layers._getMapboxLayerExists(this.currentLayerId);
 
         if (doesLayerExist) {
-            this.map.layers._removeLayer(layerId)
+            this.map.layers._removeLayer(this.currentLayerId)
         }
 
-        this.currentLayer = null;
+        this.currentLayerId = null;
     },
     setFilter: function (areaName, geometry) {
         const shape = {
@@ -51,13 +49,8 @@
                 strokeWidth: 2
             });
 
-            // Apply filter to show only the matching feature by name
-            authorityLayer.setOptions({
-                filter: ['==', ['get', 'name'], areaName]
-            });
-
             this.map.layers.add(authorityLayer);
-            this.currentLayer = authorityLayer; // Store reference to current layer
+            this.currentLayerId = authorityLayer.id; // Store reference to current layer
             // take the map camera to the searched place
             this.map.setCamera({
                 bounds: bbox,
@@ -73,13 +66,15 @@
         });
         this.map.sources.add(source);
 
-        this.currentLayer = new atlas.layer.PolygonLayer(source, null, {
+        const localAuthorities = new atlas.layer.PolygonLayer(source, null, {
             'source-layer': 'local_authorities_layer',
             sourceLayer: 'local_authorities_layer',
             fillColor: 'rgba(0, 100, 255, 0.4)',
             strokeColor: '#004aad',
             strokeWidth: 2
         });
-        this.map.layers.add(this.currentLayer);
+
+        this.map.layers.add(localAuthorities);
+        this.currentLayerId = localAuthorities.id;
     }
 };

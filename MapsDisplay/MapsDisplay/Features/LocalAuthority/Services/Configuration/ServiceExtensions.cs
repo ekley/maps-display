94743d308849cdc5c8d53c60196e86bd8c1f11c1
beneki <benjamin.ekramiand@gmail.com>
2025-04-22 19:35:18 +0100

    Refactor Program.cs: Extract service registrations into ServiceExtensions file

---
+++ MapsDisplay/MapsDisplay/Features/LocalAuthority/Services/Configuration/ServiceExtensions.cs
@@ -0,0 +1,78 @@
+﻿using MapsDisplay.Client.Services;
+using MapsDisplay.Components.Account;
+using MapsDisplay.Features.LocalAuthority.Data;
+using MapsDisplay.Features.LocalAuthority.Services.Implementations;
+using MapsDisplay.Features.LocalAuthority.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Shared.Services.Interfaces;
+
+namespace MapsDisplay.Features.LocalAuthority.Services.Configuration
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRazorComponents()
+                    .AddInteractiveServerComponents()
+                    .AddInteractiveWebAssemblyComponents();
+
+            services.AddControllers();
+            services.AddHttpClient();
+            services.AddCascadingAuthenticationState();
+            services.AddScoped<IdentityUserAccessor>();
+            services.AddScoped<IdentityRedirectManager>();
+            services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+            services.AddScoped<ILocalAuthorityApiService, LocalAuthorityApiService>();
+            services.AddScoped<ILocalAuthorityService, LocalAuthorityService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = configuration["Authentication:Microsoft:ClientId"];
+                microsoftOptions.ClientSecret = configuration["Authentication:Microsoft:ClientSecret"];
+                microsoftOptions.AuthorizationEndpoint = configuration["Authentication:Microsoft:AuthorizationEndpoint"];
+                microsoftOptions.TokenEndpoint = configuration["Authentication:Microsoft:TokenEndpoint"];
+            })
+            .AddIdentityCookies();
+        }
+
+        public static void AddCustomDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+        }
+
+        public static void AddIdentityServices(this IServiceCollection services)
+        {
+            services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddSignInManager()
+                    .AddDefaultTokenProviders();
+        }
+
+        public static void AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+        {
+            var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", policy =>
+                    policy.WithOrigins(allowedOrigins)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+            });
+        }
+
+        public static void AddEmailSender(this IServiceCollection services)
+        {
+            services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+        }
+    }
+}
--- MapsDisplay/MapsDisplay/Program.cs
+++ MapsDisplay/MapsDisplay/Program.cs
@@ -1,71 +1,24 @@
-using MapsDisplay.Client.Services;
 using MapsDisplay.Components;
-using MapsDisplay.Components.Account;
-using MapsDisplay.Features.LocalAuthority.Data;
 using MapsDisplay.Features.LocalAuthority.Services.Builders;
-using MapsDisplay.Features.LocalAuthority.Services.Implementations;
-using MapsDisplay.Features.LocalAuthority.Services.Interfaces;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Shared.Services.Interfaces;
+using MapsDisplay.Features.LocalAuthority.Services.Configuration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-
-builder.Services.AddControllers();
-builder.Services.AddHttpClient();
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-builder.Services.AddScoped<ILocalAuthorityApiService, LocalAuthorityApiService>();
-builder.Services.AddScoped<ILocalAuthorityService, LocalAuthorityService>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddMicrosoftAccount(microsoftOptions =>
-    {
-        microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
-        microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
-        microsoftOptions.AuthorizationEndpoint = builder.Configuration["Authentication:Microsoft:AuthorizationEndpoint"];
-        microsoftOptions.TokenEndpoint = builder.Configuration["Authentication:Microsoft:TokenEndpoint"];
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin", policy =>
-        policy.WithOrigins(allowedOrigins)
-              .AllowAnyHeader()
-              .AllowAnyMethod());
-});
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+// Add Services to the container using extension methods
+builder.Services.AddCustomServices(builder.Configuration);
+builder.Services.AddCustomDatabase(builder.Configuration);
+builder.Services.AddIdentityServices();
+builder.Services.AddCorsPolicy(builder.Configuration);
+builder.Services.AddEmailSender();
 
 var app = builder.Build();
 
+// Build lookup.json (in Datasets folder) file if doesn't exits
 DatasetBuilder.BuildAll();
 
 app.MapControllers();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -75,21 +28,20 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Configure interactive components
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(MapsDisplay.Client._Imports).Assembly);
+   .AddInteractiveServerRenderMode()
+   .AddInteractiveWebAssemblyRenderMode()
+   .AddAdditionalAssemblies(typeof(MapsDisplay.Client._Imports).Assembly);
 
-// Add additional endpoints required by the Identity /Account Razor components.
+// Add additional Identity endpoints
 app.MapAdditionalIdentityEndpoints();
 
 app.Run();
